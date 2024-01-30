47801c1b283605289ae1bfe1c4b3ecfd8a7f4898
NorePeeters <157713696+NorePeeters@users.noreply.github.com>
2024-01-30 11:17:20 +0100

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,18 +1,50 @@
 input.onButtonPressed(Button.A, function () {
     if (true) {
         basic.showLeds(`
-            . . . . .
             # # # # #
             . . . . .
             # # # # #
             . . . . .
+            # # # # #
             `)
         basic.showLeds(`
             . . . . .
+            # # # # #
             . . . . .
-            . . . . .
-            . . . . .
+            # # # # #
             . . . . .
             `)
     }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        basic.showLeds(`
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            # . # . #
+            `)
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # . # .
+            `)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
 })
